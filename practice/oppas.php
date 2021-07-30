6f11f697bbbdeb689d482d36700e2c9bf6ed3468
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2021-07-30 15:55:37 +0600

    unnies added

--- practice/oppas.php
+++ practice/oppas.php
@@ -1,5 +1,6 @@
 <?php 
-	$names = ['Yoo', 'Hyun', 'Woo', 'Lee', 'Nam', 'Ki', 'Suk'];
+	$oppas = ['Yoo', 'Hyun', 'Woo', 'Lee', 'Nam', 'Ki', 'Suk'];
+	$unnies = ['lee na young', 'son ye jin', 'park shin hye', 'iu', 'bae suzy'];
 	print_r($names);
 
 
